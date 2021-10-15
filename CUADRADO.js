9e167ac195cb51e4474d9748b51196f358f863af
CAT uwu <88683832+CAT9030@users.noreply.github.com>
2021-10-15 18:12:28 -0500

    estructura

--- CUADRADO.js
+++ CUADRADO.js
@@ -1,14 +1,14 @@
 class CUADRADO {
-constructor (x,y,name,b,h) {
+constructor (x,y,name,b,alto) {
     this.x = x;
     this.y = y;
     this.name = name;
     this.b = 50;
-    this.h = 50;
+    this.alto = 50;
 }
 
 printCuadrado(){
     fill(180,0,0);
-    rect(this.x,this.y,this.b.this.h);
+    rect(this.x,this.y,this.b,this.alto);
 }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let cuadrado;
+let triangulo;
+let circulo;
+let rombo;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+cuadrado = new CUADRADO(75,75,"cuadrado",50,50);
+triangulo = new TRIANGULO (375,75,355,125,400,125,"triangulo");
+circulo = new CIRCULO (125,375,"circulo",50);
+rombo = new ROMBO (375,325,340,375,375,425,410,375,"rombo");
+
+
 }
 
 function draw() {
-  background(220);
+  background(10);
+
+  stroke(250);
+  strokeWeight(3);
+  line(250,0,250,500);
+  line(0,250,500,250);
+
+  cuadrado.printCuadrado();
+  triangulo.printTriangulo();
+  circulo.printCirculo();
+  rombo.printRombo();
+
 }
